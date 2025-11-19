f728bc3f641ca10f40c4185235581640e3807f90
Andrey Merkulov <merkulov.ag@intechs.by>
2025-11-19 19:49:43 +0300

    11-geolocation

--- app.js
+++ app.js
@@ -1,45 +1,31 @@
 'use strict';
 
-function request() {
-  fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('request error');
-      }
-      return response.json();
-    })
-    .then((data) => {
-      if (!data || !data.abilities || !Array.isArray(data.abilities)) {
-        throw new Error('Invalid response structure');
-      }
+function getCurrentPositionPromise() {
+  return new Promise((resolve, rejected) => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
 
-      return fetch(data.abilities[0].ability.url);
-    })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('request error');
-      }
+        resolve({
+          latitude,
+          longitude,
+        });
+      },
 
-      return response.json();
-    })
-    .then((data1) => {
-      if (
-        !data1 ||
-        !data1.effect_entries ||
-        !Array.isArray(data1.effect_entries)
-      ) {
-        throw new Error('Invalid response structure');
+      (error) => {
+        rejected(error);
       }
-
-      const effectEntry = data1.effect_entries.find(
-        (item) => item.language.name === 'en'
-      );
-
-      if (effectEntry) {
-        console.log(effectEntry.effect);
-      }
-    })
-    .catch((error) => error.message);
+    );
+  });
 }
 
-request();
+getCurrentPositionPromise()
+  .then((data) => {
+    console.log('Position:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error.message);
+  })
+  .finally(() => {
+    console.log('Request complete');
+  });
