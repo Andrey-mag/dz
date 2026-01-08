d9df5c0d9098a7eed70fd7ad9cefe36a339e8470
Andrey Merkulov <merkulov.ag@intechs.by>
2026-01-08 15:43:25 +0300

    12-promise-race

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 'use strict';
 
-const array = [1, 2, 3];
-
-async function race(array) {
+function race(array) {
   return new Promise((resolve, reject) => {
     for (const item of array) {
       item.then(resolve).catch(reject);
