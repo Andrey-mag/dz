2c03ce4d4617c2aef42b4f54e976676cfa5cd18e
Andrey Merkulov <merkulov.ag@intechs.by>
2026-01-08 15:38:00 +0300

    12-promise-race

--- app.js
+++ app.js
@@ -1,31 +1,11 @@
 'use strict';
 
-function getCurrentPositionPromise() {
-  return new Promise((resolve, rejected) => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
+const array = [1, 2, 3];
 
-        resolve({
-          latitude,
-          longitude,
-        });
-      },
-
-      (error) => {
-        rejected(error);
-      }
-    );
+async function race(array) {
+  return new Promise((resolve, reject) => {
+    for (const item of array) {
+      item.then(resolve).catch(reject);
+    }
   });
 }
-
-getCurrentPositionPromise()
-  .then((data) => {
-    console.log('Position:', data);
-  })
-  .catch((error) => {
-    console.error('Error:', error.message);
-  })
-  .finally(() => {
-    console.log('Request complete');
-  });
